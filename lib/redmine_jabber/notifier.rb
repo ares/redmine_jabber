48d81b39f7eda4fe976e2dddf0a7d34b0c78e5f4
Marek Hulan <ares@igloonet.cz>
2012-11-13 08:53:06 +0100

    Redmine 2.1 settings issue

--- lib/redmine_jabber/notifier.rb
+++ lib/redmine_jabber/notifier.rb
@@ -2,7 +2,7 @@
 require 'blather/client/client'
 class RedmineJabber::Notifier < Blather::Client
   def self.send_message(jid, message)
-    settings = Setting.plugin_redmine_jabber
+    settings = Setting.plugin_redmine_jabber.with_indifferent_access
     client = self.setup(settings[:bot_jid], settings[:bot_password])
     client.register_handler(:ready) do
       client.write Blather::Stanza::Message.new(jid, message)
